45380b404fcafac3fbed40f6f205bcdfdd57df50
Ruben <rubsanang@alu.edu.gva.es>
2025-06-05 08:31:33 +0200

    Delete Programacion/ConexionDB1/ConexionDB/Programa/BaseDatos/Main.java

--- Programacion/ConexionDB1/ConexionDB/Programa/BaseDatos/Main.java
+++
@@ -1,9 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        ClienteDB.insertarCliente(10, "carlos@mail.com", "Juan", "Calle 123");
-        ClienteDB.mostrarClientes();
-        ClienteDB.buscarClientePorId(1);  
-        ClienteDB.borrarClientePorId(10);
-
-    }
-}
