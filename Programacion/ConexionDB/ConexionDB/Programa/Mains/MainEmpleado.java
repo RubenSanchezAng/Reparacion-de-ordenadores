3166eaa46158fc74ea3f393eb660dcb7152e996c
Ruben <rubsanang@alu.edu.gva.es>
2025-06-06 08:46:22 +0200

    Add files via upload

---
+++ Programacion/ConexionDB/ConexionDB/Programa/Mains/MainEmpleado.java
@@ -0,0 +1,37 @@
+package Mains;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+import BaseDatos.ConexionDB;
+import BaseDatos.EmpleadoDB;
+
+public class MainEmpleado {
+    public static void main(String[] args) {
+        try (Connection con = ConexionDB.obtenerConexion()) {
+                System.out.println("Conexión establecida.");
+
+            EmpleadoDB.insertarEmpleado(con, 100, "Gerente", "Juan Perez", "123456789");
+
+            EmpleadoDB.verTodosEmpleados(con);
+
+            EmpleadoDB.verEmpleadoPorId(con, 100);
+
+         
+            EmpleadoDB.modificarCargo(con, 100, "Director General");
+
+            EmpleadoDB.modificarTelefono(con, 100, "555-1234");
+
+            EmpleadoDB.modificarNombre(con, 100, "Juan Carlos Perez");
+
+            EmpleadoDB.borrarEmpleadoPorId(con, 100);
+
+            EmpleadoDB.borrarEmpleadoPorId(con, 5);
+            
+
+            } catch (SQLException e) {
+                System.out.println("Error en la base de datos");
+                e.printStackTrace();
+            }
+    }
+}
---
+++ Programacion/ConexionDB/ConexionDB/Programa/Mains/MainEquipo.java
@@ -0,0 +1,48 @@
+
+public class MainEquipo {
+    public static void main(String[] args) {
+        try (Connection con = ConexionDB.obtenerConexion()) {
+            System.out.println("Conexión establecida.");
+   
+            int id = 100;
+            String estado = "activo";
+            Date fecha = Date.valueOf(LocalDate.of(2025, 6, 6));
+            String modelo = "X123";
+            String marca = "HP";
+            int idTecnico = 3;
+            
+            EquipoDB.insertarEquipo(con, id, estado, fecha, modelo, marca, idTecnico);
+
+            // Ver todos los equipos
+            //EquipoDB.verEquipos(con);
+
+            // Ver equipo por ID
+            //EquipoDB.verEquipoPorId(con, id);
+
+            // Modificar estado
+           // EquipoDB.modificarEstado(con, id, "en reparación");
+
+            // Modificar modelo
+           // EquipoDB.modificarModelo(con, id, "Z987");
+
+            // Modificar marca
+           // EquipoDB.modificarMarca(con, id, "Lenovo");
+
+            // Modificar fecha
+           // EquipoDB.modificarFecha(con, id, Date.valueOf(LocalDate.of(2025, 6, 10)));
+
+            // Ver equipo después de modificaciones
+           // EquipoDB.verEquipoPorId(con, id);
+
+            // Borrar equipo
+           // EquipoDB.borrarEquipoPorId(con, id);
+
+            // Verificar que se borró
+            //EquipoDB.verEquipoPorId(con, id);
+
+        } catch (SQLException e) {
+            System.out.println("Error en la base de datos");
+            e.printStackTrace();
+        }
+    }
+}
